df98f0118c2ab2a031e49b218953778bbe6c8a14
Khen Solomon Lethil <khensolomon@gmail.com>
2019-12-24 09:56:22 +0100

    title, keywords, description

--- routes/about.js
+++ routes/about.js
@@ -1,21 +1,17 @@
 const app = require('..');
-// const {utility} = app.Common;
 const assist = require('../assist');
-const {dictionaries,locale} = app.Config;
+const {locale} = app.Config;
 const routes = app.Router();
 
-assist.visits();
-
 routes.get('/', function(req, res,next) {
-  // var start = utility.timeCheck();
-  // res.locals.dictionaries_total = Object.keys(dictionaries_delete).map(continental => Object.keys(dictionaries_delete[continental]).length).reduce((a, b) => a + b,0);
-  res.locals.dictionaries_total = dictionaries.map(continental => continental.lang.length).reduce((a, b) => a + b,0);
-  res.locals.visits = app.Config.visits;
-
   res.render('about', {
     title:'About',
     keywords:'Myanmar dictionary, Burmesisk ordbok, Myanmar definition, Burmese, norsk ordbok, burmissk',
-    description: 'About MyOrdbok, Free online Myanmar dictionaries', dictionaries:dictionaries
+    description: 'About MyOrdbok, Free online Myanmar dictionaries',
+    dictionaries: assist.getLangList(),
+    visits: assist.visits(),
+    locale_total: locale.length,
+    dictionaries_total: assist.getLangCount()
   })
 });
 
