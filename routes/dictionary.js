b95ca4283a2afe76dcbad8da7fda4667229052fb
Khen Solomon Lethil <khensolomon@gmail.com>
2019-12-24 09:57:21 +0100

    title, keywords, description
    - switch assist api

--- routes/dictionary.js
+++ routes/dictionary.js
@@ -1,9 +1,14 @@
 const app = require('..');
-const {dictionaries} = app.Config;
+const assist = require('../assist');
 const routes = app.Router();
 
-routes.get('/', function(req, res, next) {
-  res.render('dictionary', { title: 'MyOrdbok', dictionaries:dictionaries});
+routes.get('/', function(req, res) {
+  res.render('dictionary', {
+    title: res.locals.sol.name,
+    keywords:'0, Myanmar, dictionary'.replace(0,res.locals.sol.name),
+    description:'0 to Myanmar dictionary'.replace(0,res.locals.sol.name),
+    dictionaries: assist.getLangList()
+  });
 });
 
 module.exports = routes;
