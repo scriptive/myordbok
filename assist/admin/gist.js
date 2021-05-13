c04a5f8c331372784c901c5cf46072691b681c84
Khen Solomon Lethil <khensolomon@gmail.com>
2021-05-13 10:27:54 +0200

    Update gist

--- assist/admin/gist.js
+++ assist/admin/gist.js
@@ -4,7 +4,6 @@
 
 import {ask} from 'lethil';
 import {config} from '../anchor/index.js';
-import * as fs from 'fs';
 
 const {gistToken, gistId} = config.setting;
 
@@ -26,21 +25,27 @@ export async function get(req){
   // return abc;
   return await gist.delete('tmp.json');
 }
-// export async function get(req){
-//   // https://gist.githubusercontent.com/khensolomon/c657c5ba9308e9cae81c8d8a748c26ec/raw/c4ebda9216484ec963245439456e44fdc98ac48d/test.json
-//   var abc = await gist.get();
-//   // var result =[];
-//   // for (const item in abc.files) {
-//   //   result.push({item:abc.files[item].filename,truncated:abc.files[item].truncated,url:abc.files[item].raw_url})
-//   // }
-//   // return result;
-//   return abc;
-// }
 
 /**
  * @param {any} req
  */
-export async function set(req){
+export async function list(req){
+  var abc = await gist.get();
+
+  return abc;
+}
+
+/**
+ * @param {any} req
+ */
+export async function patch(req){
   const postData = JSON.stringify({updates:false, dates: '10.10.21'},null,2);
-  return await gist.patch('tmp.json',postData);
+  return await gist.patch('deleteme.json',postData);
+}
+
+/**
+ * @param {any} req
+ */
+export async function remove(req){
+  return await gist.delete('deleteme.json');
 }
